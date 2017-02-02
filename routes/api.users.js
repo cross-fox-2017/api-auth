9183908b142861f8e44514f8fad3730d59661aef
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-02 18:33:34 +0700

    update router with middleware

--- routes/api.users.js
+++ routes/api.users.js
@@ -6,10 +6,10 @@ var middleware = require('../controllers/middleware')
 /* localhost:3000/api/users | show all data user */
 router.get('/', middleware.decodeToken, middleware.validateUser, users.getAllUser)
 /* localhost:3000/api/users/:id | show one data user */
-router.get('/:id', users.getOneUser)
+router.get('/:id', middleware.decodeToken, middleware.validateUser, users.getOneUser)
 /* localhost:3000/api/users/:id | delete one data user */
-router.delete('/:id', users.deleteOneUser)
+router.delete('/:id', middleware.decodeToken, middleware.validateUser, users.deleteOneUser)
 /* localhost:3000/api/users:id | edit one data user */
-router.put('/:id', users.editOneUser)
+router.put('/:id', middleware.decodeToken, middleware.validateUser, users.editOneUser)
 
 module.exports = router
