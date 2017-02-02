712c4d87fddbcf198419d6f851363e2d79d1f550
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-02 17:25:44 +0700

    Testing Signup and Register

--- controller/user.js
+++ controller/user.js
@@ -22,7 +22,7 @@ module.exports = {
           if(result){
             if(passwordHash.verify(req.body.password,result.password)==true){
               //iat : session token berakhir dalam 30 detik
-              res.send(jwt.sign({id: result.id, iat: Math.floor(Date.now() / 1000)-30},"CODEuntukDECODE"))
+              res.send(jwt.sign({id: result.id, role: req.body.role, expiresIn: '1h'},"CODEuntukDECODE"))
             }else{
               res.send("Periksa Kembali User dan Pasword")
             }
@@ -31,45 +31,81 @@ module.exports = {
           }
         })
     },
-
     getAllData: function(req, res, next) {
-        models.User.findAll().then(function(data) {
+      jwt.verify(req.params.token,'CODEuntukDECODE', function(err, decoded) {
+        console.log(decoded);
+        if(decoded.role == "admin"){
+          models.User.findAll().then(function(data) {
             res.send(data)
-        })
+          })
+        }else if(decoded.role == "user"){
+            res.send("Halaman Hanya Bisa Diakses Oleh Admin")
+        }else{
+            res.send("Silahkan Login Terlebih Dahulu")
+        }
+      })
     },
     getById: function(req, res, next) {
-        models.User.findById(req.params.id).then(function(data) {
-            res.send(data)
-        })
+      jwt.verify(req.params.token,'CODEuntukDECODE', function(err, decoded) {
+        if(decoded.role.toUpperCase() == "ADMIN" || decoded.role.toUpperCase() == "USER"){
+          models.User.findById(req.body.id).then(function(data) {
+              res.send(data)
+          })
+        }else{
+            res.send("Silahkan Login Terlebih Dahulu")
+        }
+      })
     },
     createUser: function(req, res, next) {
-        models.User.create({
-            user: req.body.user,
-            password: passwordHash.generate(req.body.password),
-            role: req.body.role || "user"
-        }).then(function(data) {
-            res.send(data)
-        })
+      jwt.verify(req.params.token,'CODEuntukDECODE', function(err, decoded) {
+        if(decoded.role.toUpperCase() == "ADMIN"){
+          models.User.create({
+              user: req.body.user,
+              password: passwordHash.generate(req.body.password),
+              role: req.body.role || "user"
+          }).then(function(data) {
+              res.send(data)
+          })
+        }else if(decoded.role.toUpperCase() == "USER"){
+            res.send("Halaman Hanya Bisa Diakses Oleh Admin")
+        }else{
+            res.send("Silahkan Login Terlebih Dahulu")
+        }
+      })
     },
     delete: function(req, res, next) {
-        var tampilData
-        models.User.findById(req.params.id).then(function(data) {
-            tampilData = data
-            data.destroy()
-        }).then(function() {
-            // res.send("Data Terhapus")
-            res.send(tampilData)
-        })
+      jwt.verify(req.params.token,'CODEuntukDECODE', function(err, decoded) {
+        if(decoded.role.toUpperCase() == "ADMIN"){
+          var tampilData
+          models.User.findById(req.params.id).then(function(data) {
+              tampilData = data
+              data.destroy()
+          }).then(function() {
+              // res.send("Data Terhapus")
+              res.send(tampilData)
+          })
+        }else if(decoded.role.toUpperCase() == "USER"){
+            res.send("Halaman Hanya Bisa Diakses Oleh Admin")
+        }else{
+            res.send("Silahkan Login Terlebih Dahulu")
+        }
+      })
     },
     updateData: function(req, res, next) {
-        models.User.findById(req.params.id).then(function(data) {
-            data.update({
-                user: req.body.user,
-                password: passwordHash.generate(req.body.password),
-                role: req.body.role || "user"
-            }).then(function(showData) {
-                res.send(showData)
-            })
-        })
+      jwt.verify(req.params.token,'CODEuntukDECODE', function(err, decoded) {
+        if(decoded.role.toUpperCase() == "ADMIN" || decoded.role.toUpperCase() == "USER"){
+          models.User.findById(req.body.id).then(function(data) {
+              data.update({
+                  user: req.body.user,
+                  password: passwordHash.generate(req.body.password),
+                  role: req.body.role || "user"
+              }).then(function(showData) {
+                  res.send(showData)
+              })
+          })
+        }else{
+            res.send("Silahkan Login Terlebih Dahulu")
+        }
+      })
     }
 }
--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const users = require('../controller/user')
 
 /* GET users listing. */
-router.get('/', users.getAllData);
-router.get('/:id', users.getById);
+router.post('/signup',users.signup);
+router.post('/signin',users.signin);
+router.get('/:token', users.getAllData);
+router.post('/:id', users.getById);
 router.post('/', users.createUser);
 router.delete('/:id', users.delete);
 router.put('/:id', users.updateData);
-router.post('/signup',users.signup);
-router.post('/signin',users.signin);
+
 
 
 
